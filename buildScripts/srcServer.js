f1b767aa941683c674b01e3a2c88e959cf3c8fb5
Arthkate <arthurkateeba@gmail.com>
2020-07-25 23:00:49 +0300

    fixed error, replaced = with : on line 12

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,16 +8,18 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(require("webpack-dev-middleware")(compiler, {
-  noInfo = true,
-  publicPath: config.output.publicPath
-}));
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+);
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./src/index.html"));
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
